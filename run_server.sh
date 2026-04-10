4c046b208afd6d63121d01a4ad06cae0e1b06680
dwr-psandhu <psandhu@water.ca.gov>
2026-04-10 14:12:16 -0700

    fix: update pip install command to remove editable flag

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
-pip install --no-deps -e .
+pip install --no-deps .
 #pip install panel==1.5.0b3 # FIXME: remove this line when panel 1.5.0 is released
 #python cdec_maps/cdec_cache_build.py &
 panel serve cdec.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
--- run_server_cache_build.sh
+++ run_server_cache_build.sh
@@ -1,6 +1,6 @@
 #pip install panel==1.5.0b3 # FIXME: remove this line when panel 1.5.0 is released
 echo "pip installing this directory: $(pwd)"
-pip install --no-deps -e .
+pip install --no-deps .
 echo "starting cdec_cache_build.py"
 python cdec_maps/cdec_cache_build.py & # FIXME: move this to a background process as it locks up the cache db
 echo "starting panel serve cdecmap_builder.py"
